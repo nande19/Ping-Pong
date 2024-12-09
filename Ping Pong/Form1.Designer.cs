262d2f2b42809a6290253ea3913f8d6b0f24a1c7
ST10082757 <ST10082757@vcconnect.edu.za>
2024-12-09 23:13:06 +0200

    Add project files.

---
+++ Ping Pong/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace Ping_Pong
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.player = new System.Windows.Forms.PictureBox();
+            this.computer = new System.Windows.Forms.PictureBox();
+            this.ball = new System.Windows.Forms.PictureBox();
+            this.gameTmr = new System.Windows.Forms.Timer(this.components);
+            this.startBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.computer)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // player
+            // 
+            this.player.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.player.Location = new System.Drawing.Point(12, 65);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(28, 101);
+            this.player.TabIndex = 0;
+            this.player.TabStop = false;
+            // 
+            // computer
+            // 
+            this.computer.BackColor = System.Drawing.Color.Purple;
+            this.computer.Location = new System.Drawing.Point(353, 65);
+            this.computer.Name = "computer";
+            this.computer.Size = new System.Drawing.Size(28, 101);
+            this.computer.TabIndex = 1;
+            this.computer.TabStop = false;
+            // 
+            // ball
+            // 
+            this.ball.Image = global::Ping_Pong.Properties.Resources.Basketball_Ball_PNG_Clipart;
+            this.ball.Location = new System.Drawing.Point(162, 102);
+            this.ball.Name = "ball";
+            this.ball.Size = new System.Drawing.Size(30, 30);
+            this.ball.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ball.TabIndex = 2;
+            this.ball.TabStop = false;
+            // 
+            // gameTmr
+            // 
+            this.gameTmr.Enabled = true;
+            this.gameTmr.Interval = 20;
+            this.gameTmr.Tick += new System.EventHandler(this.GameTimer);
+            // 
+            // startBtn
+            // 
+            this.startBtn.BackColor = System.Drawing.Color.Green;
+            this.startBtn.Location = new System.Drawing.Point(141, 102);
+            this.startBtn.Name = "startBtn";
+            this.startBtn.Size = new System.Drawing.Size(100, 36);
+            this.startBtn.TabIndex = 6;
+            this.startBtn.Text = "Start";
+            this.startBtn.UseVisualStyleBackColor = false;
+            this.startBtn.Click += new System.EventHandler(this.Startbtn);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(393, 270);
+            this.Controls.Add(this.startBtn);
+            this.Controls.Add(this.ball);
+            this.Controls.Add(this.computer);
+            this.Controls.Add(this.player);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Player: 0 -- Computer: 0";
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.computer)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.PictureBox computer;
+        private System.Windows.Forms.PictureBox ball;
+        private System.Windows.Forms.Timer gameTmr;
+        private System.Windows.Forms.Button startBtn;
+    }
+}
+
---
+++ Ping Pong/Form1.cs
@@ -0,0 +1,349 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ping_Pong
+{
+    public partial class Form1 : Form
+    {
+        // Variables to track mouse dragging for player and computer picture boxes
+
+        private bool isDragging = false;
+        private Point lastLocation;
+        private bool isComputerDragging = false;
+        private Point computerLastLocation;
+
+        // Variables to control the movement of the ball
+
+        int ballXSpeed = 4;
+        int ballYSpeed = 4;
+        int speed = 6; // Speed of computer paddle movement
+        private readonly Random RandomNumber = new Random();
+        int computerSpeedChange = 50;// Speed change interval for computer paddle movement
+                                     
+        // Variables to keep track of player and computer scores
+        int playerScore = 0;
+        int computerScore = 0;
+
+        // Variables related to player and computer paddle speeds
+        int playerSpeed = 8;
+        int[] a = { 5, 6, 8, 9 };
+        int[] b = { 10, 9, 8, 11, 12 };
+
+        //--------------------------------------------------------------------------------------------------------//
+        /// <summary>
+        /// Constructor 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Set the minimum and maximum sizes to make the form fixed in height and resizable in width
+            int size = 400; // Set the desired size for both width and height
+            this.MinimumSize = new Size(size, size);
+            this.MaximumSize = new Size(size, size);
+
+            // Set the initial location of the computer picture box
+            int computerX = size - 40; // Adjust the value as needed
+            int computerY = size / 2 - 25; // Adjust the value as needed
+            computer.Location = new Point(computerX, computerY);
+
+            // Event handlers for mouse actions on player and computer picture boxes
+            player.MouseDown += Player_MouseDown;
+            player.MouseMove += Player_MouseMove;
+            player.MouseUp += Player_MouseUp;
+            computer.MouseDown += Computer_MouseDown;
+            computer.MouseMove += Computer_MouseMove;
+            computer.MouseUp += Computer_MouseUp;
+
+            // Disable the game timer initially
+            gameTmr.Enabled = false;
+
+            // Enable double buffering for the form to reduce flickering during rendering
+            this.DoubleBuffered = true;
+        }
+        //--------------------------------------------------------------------------------------------------------//
+        /// <summary>
+        /// Event handler for Start button click
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+
+        private void Startbtn(object sender, EventArgs e)
+        {
+            // Enable the game timer
+            gameTmr.Enabled = true;
+
+            // Hide the start button
+            startBtn.Visible = false;
+        }
+        //--------------------------------------------------------------------------------------------------------//
+
+        /// <summary>
+        /// Game timer event handler
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void GameTimer(object sender, EventArgs e)
+        {
+            // Flag to track if a score has been recorded in the current cycle
+            bool scoreRecorded = false;
+
+            // Update ball position based on its speed
+            ball.Top -= ballYSpeed;
+            ball.Left -= ballXSpeed;
+
+            // Update player and computer scores display
+            this.Text = "Player Score: " + playerScore + "-- Computer Score: " + computerScore;
+
+            // Check for collision with top or bottom boundary
+            if (ball.Top < 0 || ball.Bottom > this.ClientSize.Height)
+            {
+                ballYSpeed = -ballYSpeed;
+            }
+
+            // Check for collision with left boundary and update computer score
+            if (ball.Left < -2 && !scoreRecorded)
+            {
+                ball.Left = 300;
+                ballXSpeed = -ballXSpeed;
+                computerScore++;
+                scoreRecorded = true; // Mark score as recorded to avoid multiple updates
+            }
+
+            // Check for collision with right boundary and update player score
+            if (ball.Right > this.ClientSize.Width + 2 && !scoreRecorded)
+            {
+                ball.Left = 300;
+                ballXSpeed = -ballXSpeed;
+                playerScore++;
+                scoreRecorded = true; // Mark score as recorded to avoid multiple updates
+            }
+
+            // Limit computer paddle movement within form boundaries
+            if (computer.Top <= 1)
+            {
+                computer.Top = 0;
+            }
+            else if (computer.Bottom >= this.ClientSize.Height)
+            {
+                computer.Top = this.ClientSize.Height - computer.Height;
+            }
+
+            // Adjust computer paddle movement based on ball position
+            if (ball.Top < computer.Top + (computer.Height / 2) && ball.Left > 300)
+            {
+                computer.Top -= speed;
+            }
+            if (ball.Top < computer.Top + (computer.Height / 2) && ball.Left > 300)
+            {
+                computer.Top -= speed;
+            }
+            if (ball.Top < computer.Top + (computer.Height / 2) && ball.Left > 300)
+            {
+                computer.Top += speed;
+            }
+
+            // Adjust computer paddle movement speed at regular intervals
+            computerSpeedChange -= 1;
+
+            if (computerSpeedChange < 0)
+            {
+                speed = a[RandomNumber.Next(a.Length)];
+                computerSpeedChange = 50;
+            }
+
+            // Check collision between ball and player/computer paddles
+            CheckCollision(ball, player, player.Right + 5);
+            CheckCollision(ball, computer, computer.Left - 35);
+
+            // Check for winner and display game over message
+            if (computerScore >= 4)
+            {
+                GameOverDisplay("LOSER!");
+            }
+            else if (playerScore >= 4)
+            {
+                GameOverDisplay("WINNER!");
+            }
+        }
+
+        //--------------------------------------------------------------------------------------------------------//
+
+        /// <summary>
+        /// Method to check collision between two picture boxes
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        /// 
+        private void CheckCollision(PictureBox pic1, PictureBox pic2, int offset)
+        {
+            if (pic1.Bounds.IntersectsWith(pic2.Bounds))
+            {
+                pic1.Left = offset;
+
+                int x = a[RandomNumber.Next(a.Length)];
+                int y = a[RandomNumber.Next(a.Length)];
+
+                if (ballXSpeed < 0)
+                {
+                    ballXSpeed = x;
+                }
+                else
+                {
+                    ballXSpeed = -x;
+                }
+
+                if (ballXSpeed < 0)
+                {
+                    ballXSpeed = -y;
+                }
+                else
+                {
+                    ballXSpeed = y;
+                }
+            }
+        }
+
+
+        //--------------------------------------------------------------------------------------------------------//
+
+        /// <summary>
+        /// Event handler for mouse down on player paddle
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+
+        private void Player_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                if (player.ClientRectangle.Contains(e.Location))
+                {
+                    isDragging = true;
+                    lastLocation = e.Location;
+                }
+            }
+        }
+        //--------------------------------------------------------------------------------------------------------//
+        /// <summary>
+        /// Event handler for mouse move on player paddle
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Player_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDragging)
+            {
+                int deltaY = e.Y - lastLocation.Y;
+                int newY = player.Top + deltaY;
+
+                // Limit the movement to stay within the bounds of the form
+                newY = Math.Max(0, Math.Min(newY, this.ClientSize.Height - player.Height));
+
+                player.Top = newY;
+            }
+        }
+        //--------------------------------------------------------------------------------------------------------//
+        /// <summary>
+        /// Event handler for mouse up on player paddle
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Player_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                isDragging = false;
+            }
+        }
+
+        //--------------------------------------------------------------------------------------------------------//
+
+        /// <summary>
+        /// Event handler for mouse down on computer paddle
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+
+
+        private void Computer_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                if (computer.ClientRectangle.Contains(e.Location))
+                {
+                    isComputerDragging = true;
+                    computerLastLocation = e.Location;
+                }
+            }
+        }
+
+        //--------------------------------------------------------------------------------------------------------//
+
+        /// <summary>
+        /// Event handler for mouse move on computer paddle
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Computer_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isComputerDragging)
+            {
+                int deltaY = e.Y - computerLastLocation.Y;
+                int newY = computer.Top + deltaY;
+
+                // Limit the movement to stay within the bounds of the form
+                newY = Math.Max(0, Math.Min(newY, this.ClientSize.Height - computer.Height));
+
+                computer.Top = newY;
+            }
+        }
+
+        //--------------------------------------------------------------------------------------------------------//
+
+        /// <summary>
+        /// Event handler for mouse up on computer paddle
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Computer_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                isComputerDragging = false;
+            }
+        }
+
+
+        //--------------------------------------------------------------------------------------------------------//
+        /// <summary>
+        /// Method to display game over message and reset game variables
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+
+        private void GameOverDisplay(string message)
+        {
+            gameTmr.Stop();
+            MessageBox.Show(message, "Look: ");
+            computerScore = 0;
+            playerScore = 0;
+            ballXSpeed = ballYSpeed = 4;
+            computerSpeedChange = 50;
+            gameTmr.Start();
+
+            }
+       
+    }
+}
+        //---------------------------------------- END OF FILE -------------------------------------------------------//
